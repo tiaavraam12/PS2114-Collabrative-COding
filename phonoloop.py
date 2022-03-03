8cb1713a5e2ae5fa60853032de3eeef98e828ba8
tiaavraam12 <100858032+tiaavraam12@users.noreply.github.com>
2022-03-03 15:20:42 +0000

    Update phonoloop.py

--- phonoloop.py
+++ phonoloop.py
@@ -2,7 +2,7 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-
+import numpy.random as rnd
 hrRange = np.array([1.64, 1.92, 2.33, 2.64, 3.73, 3.94]) 
 # Human speech rate
 hdata = np.array([0.31, 0.47, 0.51, 0.7, 0.72, 0.8])     
@@ -12,7 +12,8 @@ nReps = 1000       # Number of reps
 
 listLength = 5    # List length
 initAct = 1       # Initial activation after study
-dRate = 0.8       # Decay rate (per second)
+decRate = 0.8     # Mean decay rate (per second)
+decSD = 0.1       # Standard deviation of decay
 delay = 5.        # Retention interval (seconds)
 minAct = 0.0      # Minimum activation needed to recall
 
@@ -24,6 +25,7 @@ pCor = np.zeros(len(rRange))
 i=0  # Index for word lengths.
 for tPerWord in tRange:
     for rep in range(nReps):
+       dRate = decRate+rnd.randint(0,3)*decSD
        actVals = np.ones(listLength)*initAct
        cT = 0.0
        itemReh = -1 # Start rehearsal with beginning of list
